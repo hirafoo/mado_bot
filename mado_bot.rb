60b4de1fbd9a36b231577e48ead92917a39ae2d0
hirafoo <hirafoo@cpan.org>
2010-12-03 15:39:31 +0900

    ignore user

--- mado_bot.rb
+++ mado_bot.rb
@@ -64,6 +64,7 @@ class MadoBot
       twit.from_user != 'fx_fan_jp' and
       twit.from_user != 'abu_mustafa' and
       twit.from_user != 'take_cheeze' and
+      twit.from_user != 'ko10buki' and
       /[一二三四五六七八九]窓/ !~ str and
       /ジョハリの窓/ !~ str and
       /[０-９]窓/ !~ str and
